79b5412e9ea8cf8bf5998d92220feaf57e570ea4
Priceless Brewing <shadowchao99@gmail.com>
2015-09-29 20:41:49 -0500

    Update Testing5.js

--- BiabCalc/Calc/Testing5.js
+++ BiabCalc/Calc/Testing5.js
@@ -4,6 +4,7 @@
       'BatchVol',
       'GBill',
       'HBill',
+      'DHop',
       'BoilTime',
       'BoilRate',
       'TempGrain',
@@ -12,6 +13,9 @@
       'PotSize',
       'KettleID',
       'LossTrub',
+      'LossFermTrub',
+      'HChilled',
+      'VolPackaged',
       'Gabs',
       'Habs'
     ];
@@ -29,6 +33,7 @@
     var BatchVol = parseFloat($('#BatchVol').val()),
     GBill = parseFloat($('#GBill').val()),
     HBill = parseFloat($('#HBill').val()),
+    DHop = parseFloat($('#DHop').val()),
     BoilTime = parseFloat($('#BoilTime').val()),
     BoilRate = parseFloat($('#BoilRate').val()),
     TempGrain = parseFloat($('#TempGrain').val()),
@@ -41,6 +46,7 @@
     Habs = parseFloat($('#Habs').val()),
     MashAdj = parseFloat($('#MashAdj').val()),
     StrikeAdj = parseFloat($('#StrikeAdj').val()),
+    LossFermTrub = parseFloat($('#LossFermTrub').val()),
     LossBoil = BoilTime * BoilRate / 60,
     LossHop = HBill * Habs,
     LossGrain = GBill * Gabs,
@@ -48,50 +54,60 @@
     WaterTot = BatchVol + LossTot,
     VolStart = (WaterTot - VolSparge),
     TempStrike = TempMash + (0.05 * GBill / VolStart) * (TempMash - TempGrain),
-    
-    MashAdj = ( 4.13643 * Math.pow(10,-16) * Math.pow($('#TempMash').val(),6) - 4.05998 * Math.pow(10,-13) * Math.pow($('#TempMash').val(),5) + 1.61536 * Math.pow(10,-10) * Math.pow($('#TempMash').val(),4) - 3.44854 * Math.pow(10,-8) * Math.pow($('#TempMash').val(),3) + 0.00000532769 * Math.pow($('#TempMash').val(),2) - 0.000292675 * $('#TempMash').val() + 1.00493),  
-    
-    StrikeAdj = ( 4.13643 * Math.pow(10,-16) * Math.pow($('#TempStrike').val(),6) - 4.05998 * Math.pow(10,-13) * Math.pow($('#TempStrike').val(),5) + 1.61536 * Math.pow(10,-10) * Math.pow($('#TempStrike').val(),4) - 3.44854 * Math.pow(10,-8) * Math.pow($('#TempStrike').val(),3) + 0.00000532769 * Math.pow($('#TempStrike').val(),2) - 0.000292675 * $('#TempStrike').val() + 1.00493), 
-    
+    MashAdj = 1.022494888,
+    //( 4.13643 * Math.pow(10,-16) * Math.pow($('#TempMash').val(),6) - 4.05998 * Math.pow(10,-13) * Math.pow($('#TempMash').val(),5) + 1.61536 * Math.pow(10,-10) * Math.pow($('#TempMash').val(),4) - 3.44854 * Math.pow(10,-8) * Math.pow($('#TempMash').val(),3) + 0.00000532769 * Math.pow($('#TempMash').val(),2) - 0.000292675 * $('#TempMash').val() + 1.00493),  
+    StrikeAdj = 1.025641026,
+    //( 4.13643 * Math.pow(10,-16) * Math.pow($('#TempStrike').val(),6) - 4.05998 * Math.pow(10,-13) * Math.pow($('#TempStrike').val(),5) + 1.61536 * Math.pow(10,-10) * Math.pow($('#TempStrike').val(),4) - 3.44854 * Math.pow(10,-8) * Math.pow($('#TempStrike').val(),3) + 0.00000532769 * Math.pow($('#TempStrike').val(),2) - 0.000292675 * $('#TempStrike').val() + 1.00493),
     VolStrike = VolStart * StrikeAdj,
     LossHop = HBill * Habs,
     LossGrain = GBill * Gabs,
     VolMash = (VolStart + GBill * 0.08) * MashAdj,
     VolPre = (WaterTot - LossGrain) * 1.043841336,
-    VolPost = (WaterTot - LossTot) * 1.043841336,
+    VolPost = (WaterTot - LossTot + LossTrub) * 1.043841336,
+    VolChilled = ( VolPost / 1.043841336 ) - LossTrub,
+    VolPackaged = VolChilled - LossFermTrub - ( DHop * Gabs ),
     GalH = 294.118334834 / (KettleID * KettleID),
     HTot = GalH * WaterTot,
     HStart = GalH * VolStart,
     HStrike = GalH * VolStrike,
     HMash = GalH * VolMash,
     HPre = GalH * VolPre,
+    HChilled = GalH * VolChilled,
     MashThick = VolStart * 4 / GBill,
-    VolMinSparge = ( ( WaterTot + GBill * 0.08) * MashAdj ) - ( ( PotSize -0.01 ) ), 
-    //WaterTot + ( GBill * 0.08 * MashAdj )-( PotSize - 0.01 ) / MashAdj);
-    HPost = GalH * VolPost;
+    VolMinSparge = Math.max(0,((WaterTot + GBill * 0.08) * MashAdj) - (PotSize - 0.01 )), 
+    HPost = GalH * VolPost,
+    EBoil = (0.058 * KettleID * KettleID) - (0.0009 * KettleID) + 0.0038;
+    
+    
     // console.log(VolStrike, WaterTot, MashThick, TempStrike);
-    $('#WaterTot').text(WaterTot.toPrecision(3));
-    $('#VolStrike').text(VolStrike.toPrecision(3));
-    $('#LossBoil').text(LossBoil.toPrecision(3));
-    $('#LossHop').text(LossHop.toPrecision(3));
-    $('#LossGrain').text(LossGrain.toPrecision(3));
-    $('#LossTot').text(LossTot.toPrecision(3));
-    $('#VolStart').text(VolStart.toPrecision(3));
-    $('#VolMash').text(VolMash.toPrecision(3));
-    $('#VolPre').text(VolPre.toPrecision(3));
-    $('#VolPost').text(VolPost.toPrecision(3));
-    $('#TempStrike').text(TempStrike.toPrecision(5));
-    $('#MashAdj').text(MashAdj.toPrecision(12));
-    $('#Strikeadj').text(StrikeAdj.toPrecision(12));
-    $('#GalH').text(GalH.toPrecision(5));
-    $('#HTot').text(HTot.toPrecision(5));
-    $('#HStart').text(HStart.toPrecision(5));
-    $('#HStrike').text(HStrike.toPrecision(5));
-    $('#HMash').text(HMash.toPrecision(5));
-    $('#HPre').text(HPre.toPrecision(5));
-    $('#HPost').text(HPost.toPrecision(5));
-    $('#MashThick').text(MashThick.toPrecision(3));
-    $('#VolMinSparge').text(VolMinSparge.toPrecision(3));
+    $('#WaterTot').text(WaterTot.toFixed(2));
+    $('#VolStrike').text(VolStrike.toFixed(2));
+    $('#LossBoil').text(LossBoil.toFixed(2));
+    $('#LossHop').text(LossHop.toFixed(2));
+    $('#LossGrain').text(LossGrain.toFixed(2));
+    $('#LossTot').text(LossTot.toFixed(2));
+    $('#LossFermTrub').text(LossFermTrub.toFixed(2));    
+    $('#VolStart').text(VolStart.toFixed(2));
+    $('#VolMash').text(VolMash.toFixed(2));
+    $('#VolPre').text(VolPre.toFixed(2));
+    $('#VolPost').text(VolPost.toFixed(2));
+    $('#TempStrike').text(TempStrike.toFixed(2));
+    $('#MashAdj').text(MashAdj.toFixed(12));
+    $('#Strikeadj').text(StrikeAdj.toFixed(12));
+    $('#GalH').text(GalH.toFixed(3));
+    $('#HTot').text(HTot.toFixed(2));
+    $('#HStart').text(HStart.toFixed(2));
+    $('#HStrike').text(HStrike.toFixed(2));
+    $('#HMash').text(HMash.toFixed(2));
+    $('#HPre').text(HPre.toFixed(2));
+    $('#HPost').text(HPost.toFixed(2));
+    $('#DHop').text(DHop.toFixed(2));    
+    $('#HChilled').text(HChilled.toFixed(2));
+    $('#MashThick').text(MashThick.toFixed(2));
+    $('#VolMinSparge').text(VolMinSparge.toFixed(2));
+    $('#VolChilled').text(VolChilled.toFixed(2));
+    $('#VolPackaged').text(VolPackaged.toFixed(2));
+    $('#EBoil').text(VolPackaged.toFixed(2));
   }
   function validateField(field) {
     $field = $(field);
